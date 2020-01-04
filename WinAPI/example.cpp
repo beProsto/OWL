3dde824d4f172100da875b38d5674fe6243b33dc
beProsto <lavionperavion@gmail.com>
2020-01-04 17:58:52 +0100

    Formatted the winapi/example.cpp

--- WinAPI/example.cpp
+++ WinAPI/example.cpp
@@ -2,70 +2,66 @@
 
 #include <windows.h>
 
-LPSTR ClassName = "WinClassName";
-MSG Message;
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam);
 
-LRESULT CALLBACK WndProc( HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam );
-
-int WINAPI WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow )
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
+    LPSTR ClassName = "WinClassName";
+
     WNDCLASSEX wc;
-   
-    wc.cbSize = sizeof( WNDCLASSEX );
+    wc.cbSize = sizeof(WNDCLASSEX);
     wc.style = 0;
     wc.lpfnWndProc = WndProc;
     wc.cbClsExtra = 0;
     wc.cbWndExtra = 0;
     wc.hInstance = hInstance;
-    wc.hIcon = LoadIcon( NULL, IDI_APPLICATION );
-    wc.hCursor = LoadCursor( NULL, IDC_ARROW );
-    wc.hbrBackground =( HBRUSH )( COLOR_WINDOW + 1 );
+    wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
     wc.lpszMenuName = NULL;
     wc.lpszClassName = ClassName;
-    wc.hIconSm = LoadIcon( NULL, IDI_APPLICATION );
+    wc.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
    
-    if( !RegisterClassEx( & wc ) )
+    if(!RegisterClassEx(&wc))
     {
         MessageBox(NULL, "Cannot register window", MB_ICONEXCLAMATION);
         return 1;
     }
 
-    HWND hwnd;
+    HWND hwnd = CreateWindowEx(WS_EX_CLIENTEDGE, ClassName, "Window", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, 240, 120, NULL, NULL, hInstance, NULL);
    
-    hwnd = CreateWindowEx( WS_EX_CLIENTEDGE, ClassName, "Window", WS_OVERLAPPEDWINDOW,
-    CW_USEDEFAULT, CW_USEDEFAULT, 240, 120, NULL, NULL, hInstance, NULL );
-   
-    if( hwnd == NULL )
+    if(hwnd == NULL)
     {
-        MessageBox( NULL, "Can't create window", MB_ICONEXCLAMATION );
+        MessageBox(NULL, "Can't create window", MB_ICONEXCLAMATION);
         return 1;
     }
    
-    ShowWindow( hwnd, nCmdShow );
-    UpdateWindow( hwnd );
+    ShowWindow(hwnd, nCmdShow);
+    UpdateWindow(hwnd);
    
-    while( GetMessage( & Message, NULL, 0, 0 ) )
+    MSG Message;
+    while(GetMessage(&Message, NULL, 0, 0))
     {
-        TranslateMessage( & Message );
-        DispatchMessage( & Message );
+        TranslateMessage(&Message);
+        DispatchMessage(&Message);
     }
     return Message.wParam;
 }
 
-LRESULT CALLBACK WndProc( HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam )
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
-    switch( msg )
+    switch(msg)
     {
     case WM_CLOSE:
-        DestroyWindow( hwnd );
+        DestroyWindow(hwnd);
         break;
        
     case WM_DESTROY:
-        PostQuitMessage( 0 );
+        PostQuitMessage(0);
         break;
        
-        default:
-        return DefWindowProc( hwnd, msg, wParam, lParam );
+    default:
+        return DefWindowProc(hwnd, msg, wParam, lParam);
     }
    
     return 0;
