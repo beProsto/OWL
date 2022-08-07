571b7587143d978cc99cecb43f0b1298007dccfe
beProsto <lavionperavion@gmail.com>
2022-08-07 21:13:00 +0200

    added a comment

--- include/OWL/OS/Linux.hpp
+++ include/OWL/OS/Linux.hpp
@@ -3,10 +3,11 @@
 #include <OWL/Utility/Config.hpp>
 
 #if defined OWL_SYSTEM_LINUX
+	// Most of these prolly shouldn't be included in the headers
 	#include <X11/Xlib.h>
 	#include <X11/Xutil.h>
 	#include <X11/keysymdef.h>
 	#include <X11/cursorfont.h>
 	#include <X11/Xatom.h>
-	
+
 #endif
