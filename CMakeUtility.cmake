e4c0b48833224a0da59fd411c3b9b3f1f71a2907
beProsto <lavionperavion@gmail.com>
2022-08-22 20:41:59 +0200

    polish out the cmake files

--- CMakeUtility.cmake
+++ CMakeUtility.cmake
@@ -11,6 +11,35 @@ elseif(UNIX)
 endif()
 
 
+# Determines if the library is statically compiled
+if(LIBTYPE STREQUAL "STATIC")
+	set(OWL_BUILD_STATIC TRUE)
+
+endif()
+
+
+# Determines the preffered windowing subsystem on linux
+if(DISABLE_WAYLAND STREQUAL "YES")
+	set(OWL_WAYLAND_OFF TRUE)
+	set(OWL_X11_ON TRUE)
+
+elseif(DISABLE_X11 STREQUAL "YES")
+	set(OWL_WAYLAND_ON TRUE)
+	set(OWL_X11_OFF TRUE)
+
+endif()
+
+
+# Determines if building examples is enabled
+if(BUILD_EXAMPLES STREQUAL "NO")
+	set(OWL_EXAMPLES_OFF TRUE)
+
+else()
+	set(OWL_EXAMPLES_ON TRUE)
+
+endif()
+
+
 # Sets the executable type
 if(OWL_SYSTEM_WINDOWS)
 	set(OWL_EXE_TYPE WIN32)
@@ -45,6 +74,7 @@ function(OWL_GenerateHtml name)
 	file(WRITE ${CMAKE_BINARY_DIR}/${name}.html ${base})
 endfunction()
 
+
 # Adds an executable target for our examples
 function(OWL_AddExample name)
 	file(GLOB_RECURSE ${name}_srcs
@@ -54,7 +84,7 @@ function(OWL_AddExample name)
 
 	add_executable(${name} ${OWL_EXE_TYPE} ${${name}_srcs})
 
-	if(LIBTYPE STREQUAL "STATIC")
+	if(OWL_BUILD_STATIC)
 		target_compile_definitions(${name} PRIVATE OWL_BUILD_STATIC)
 	endif()
 
