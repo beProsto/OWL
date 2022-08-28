f33e0baa7f8f66680bd65f124d9252e28ac6a3e1
beProsto <lavionperavion@gmail.com>
2022-08-28 02:28:03 +0200

    [Emscripten] funny bit of testing

--- examples/EmTemp/main.cpp
+++ examples/EmTemp/main.cpp
@@ -4,8 +4,37 @@
 #include <OWL/Main.hpp>
 #include <OWL/Time.hpp>
 
-int main(int argc, char** argv) {
-    printf("Heya, Emscripten!\n");
+#include <SDL2/SDL.h>
+
+SDL_Window *window;
+SDL_Renderer *renderer;
+SDL_Surface *surface;
+
+void drawRandomPixels() {
+    if (SDL_MUSTLOCK(surface)) SDL_LockSurface(surface);
+
+    Uint8 * pixels = (Uint8*)surface->pixels;
     
-    return 0;
+    for (int i=0; i < 1048576; i++) {
+        char randomByte = rand() % 255;
+        pixels[i] = randomByte;
+    }
+
+    if (SDL_MUSTLOCK(surface)) SDL_UnlockSurface(surface);
+
+    SDL_Texture *screenTexture = SDL_CreateTextureFromSurface(renderer, surface);
+
+    SDL_RenderClear(renderer);
+    SDL_RenderCopy(renderer, screenTexture, NULL, NULL);
+    SDL_RenderPresent(renderer);
+
+    SDL_DestroyTexture(screenTexture);
+}
+
+int main(int argc, char* argv[]) {
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_CreateWindowAndRenderer(512, 512, 0, &window, &renderer);
+    surface = SDL_CreateRGBSurface(0, 512, 512, 32, 0, 0, 0, 0);
+    
+    emscripten_set_main_loop(drawRandomPixels, 0, 1);
 }
